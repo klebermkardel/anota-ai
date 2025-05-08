8d0926423284eb3ee7837cf5e237511bf8d13db5
Kleber Molina Kardel <molinakardel@gmail.com>
2025-05-08 17:56:38 -0300

    :test_tube: test: testa a conexão no server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-/* */
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const sequelize = require('./config/db')
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('API do Anota Aí funcionando!')
+})
+
+sequelize.authenticate()
+    .then(() => {
+        console.timeLog('Conectado ao MySQL com sucesso!')
+        return sequelize.sync()
+    })
+    .then(() => {
+        app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'))
+    })
+    .catch(err => {
+        console.error('Erro ao conectador no banco', err)
+    })
